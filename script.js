e5bc14462e60a27a5e5db1677e1076a50f2bc496
gbaabdul <89810763+gbaabdul@users.noreply.github.com>
2022-01-28 20:42:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 alert("Please bear with us as our engineers are working on the issues of images on this website. Thanks for your usual cooperation.")
+alert("Hello! I am an alert box!!");
 const navbar = document.querySelector(".navbar");
 const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
